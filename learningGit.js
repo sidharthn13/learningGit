a0b75c11b2e7510ffcea2fedc4142ac2e01e51ac
sidharth <sidharth.nair@neoito.com>
2023-11-12 14:17:43 +0530

    new commit in new branch

--- learningGit.js
+++ learningGit.js
@@ -1,2 +1,4 @@
-console.log('first commit')
+//program in the new branch starts here
+console.log('first commit in the new branch')
+console.log('changes in the new branch')
 //switched to a new branch
